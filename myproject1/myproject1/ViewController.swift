622af09a95941324dc2e8c37a4ecfda56773261f
cow86sam <31614255+cow86sam@users.noreply.github.com>
2017-09-08 17:12:14 +0800

    Add files via upload

---
+++ myproject1/myproject1/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  myproject1
+//
+//  Created by mac on 2017/9/8.
+//  Copyright © 2017年 course.IOS5.lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func dissmissNext(segue:UIStoryboardSegue){
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        self.navigationController?.hidesBottomBarWhenPushed = true
+      //  self.tabBarController?.hidesBottomBarWhenPushed = true
+        
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
